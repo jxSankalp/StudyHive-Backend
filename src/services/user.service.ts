14d1e49e9306988ee5fa4afe2321403c06a2c220
akshay <akshaylabh08@gmail.com>
2025-06-06 02:18:48 +0530

    webhooks-working

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,36 +2,19 @@ import { User } from "../models/User";
 import { CreateUserParams, UpdateUserParams } from "../types/user";
 
 export const createUserInDB = async (params: CreateUserParams) => {
-    try {
-        const { clerkId, email, username, photo } = params;
-        const newUser = await User.create({ clerkId, email, username, photo });
-        return newUser;
-    } catch (error) {
-        throw error;
-    }
+  const { clerkId, email, username, photo } = params;
+  return await User.create({ clerkId, email, username, photo });
 };
 
-export const getUserByIdFromDB = async (id: string) => {
-    try {
-        return await User.findById(id);
-    } catch (error) {
-        throw error;
-    }
+export const getUserByIdFromDB = async (clerkId: string) => {
+  return await User.findOne({ clerkId });
 };
 
-export const updateUserInDB = async (id: string, params: UpdateUserParams) => {
-    try {
-        const { username, photo } = params;
-        return await User.findByIdAndUpdate(id, { username, photo }, { new: true });
-    } catch (error) {
-        throw error;
-    }
+export const updateUserInDB = async (clerkId: string, params: UpdateUserParams) => {
+  const { username, photo } = params;
+  return await User.findOneAndUpdate({ clerkId }, { username, photo }, { new: true });
 };
 
-export const deleteUserFromDB = async (id: string) => {
-    try {
-        return await User.findByIdAndDelete(id);
-    } catch (error) {
-        throw error;
-    }
+export const deleteUserFromDB = async (clerkId: string) => {
+  return await User.findOneAndDelete({ clerkId });
 };
